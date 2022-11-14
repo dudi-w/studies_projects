f695d41c710de1cd9e45a3b704dbb2514f823011
dudi-w <dudi.w9453@gmail.com>
2022-11-14 18:06:20 +0200

    end day

--- knn.py
+++ knn.py
@@ -13,7 +13,7 @@ def extract_file():
 
 
 #mix the data, and split the data to trean VS test
-def mix_split(data):
+def mix_and_split(data):
 	random.shuffle(data)
 	trean_data= data[:int(len(data)*0.8)]
 	test_data= data[int(len(data)*0.8):]
@@ -25,13 +25,38 @@ def distance_check(naw_point , trean_data)
 		for j in range(len(trean_data[i])-1)
 			sum_dis+= (trean_data[i][j]-new_point[j])**2
 		array_dist.append([sum_dis**0.5 , trean_data[i][len(trean_data[i])-1]]
+	array_dist.sort()
 	return array_dist
-		#(((trean_data[i][0]-new_point[0])**2)+((trean_data[i][1]-new_point[1])**2)+((trean_data[i][2]-new_point[2])**2)+((trean_data[i][3]-new_point[0])**2))**0.5
+	
+def label_prediction(array_dist, trashold):
+	count={}
+	i=0
+	while(i<trashold)
+		for value in array_dist:
+			if value[1] not in count:
+				count[value[1]]=1
+			else:
+				count[value[1]]+=1
+		i+=1
 		
+	most_common= max(count,key=count.get)
+	cont_most_common= count.get(most_common, "Not found"))
+	count.pop(most_common)
+	if count.get(max(count,key=count.get)) != cont_most_common:
+		return cont_most_common
+	else
+		return label_prediction(array_dist, trashold-1):
+		
+	
+
 
 def main():
-	print(mix_split(extract_file())[1][0])
-	
+	trashold=5
+	trean_data , test_data = mix_and_split(extract_file())[1][0]
+	for point in test_data:
+		vactor_dist = distance_check(point , trean_data)
+		result = label_prediction( vactor_dist , trashold)
+		print (result)
 if __name__ == '__main__':
 	main()
 #(((trean_data[0][0]-new_point[0])**2)+((trean_data[0][1]-new_point[1])**2)+((trean_data[0][2]-new_point[2])**2)+((trean_data[0][3]-new_point[0])**2))**0.5
