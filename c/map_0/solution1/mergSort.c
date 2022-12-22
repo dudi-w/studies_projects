b855feeb6ebb38051c9f1706ddfc747a679ba4b8
dudi-w <dudi.w9453@gmail.com>
2022-12-22 17:08:41 +0200

    update

--- c/map_0/solution1/mergSort.c
+++ c/map_0/solution1/mergSort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "solution1.h"
 
-int * mergeSort(int *arr1 ,int size1 ,int *arr2 , int size2){
+int * mergeSort(int *arr1 ,int size1 ,int *arr2 , int size2, int (*func)(int)){
 
     if(size1<1)
         return arr2;
@@ -14,7 +14,7 @@ int * mergeSort(int *arr1 ,int size1 ,int *arr2 , int size2){
 
     for(int i=0; i< size1+size2 ;++i){
         printf("arr1[%d] = %d ---  arr2[%d] = %d\n", j ,arr1[j], k, arr2[k]);
-        if((arr1[j]<=arr2[k] && j<size1) || k>=size2 )
+        if((func(arr1[j])<=func( arr2[k]) && j<size1) || k>=size2 )
             arr[i]=arr1[j++];
         else
             arr[i]=arr2[k++];
@@ -24,7 +24,7 @@ int * mergeSort(int *arr1 ,int size1 ,int *arr2 , int size2){
 
 }
 
-int *sort(int arr[], int start, int end){
+int *sort(int arr[], int start, int end,int (*func)(int)){
     if(end-start<=1)
         return arr;
     int *arr1 = partition(arr ,start, (end-start)/2);
@@ -35,10 +35,10 @@ int *sort(int arr[], int start, int end){
     printArray(arr2,size2);
     printf("size1 = %d\t size2 = %d\n",size1,size2);
     if (size1 >1)
-        arr1 = sort(arr1 ,start, (end-start)/2);
+        arr1 = sort(arr1 ,start, (end-start)/2, func);
     if(size2>1)
-        arr2 = sort(arr2,0, size2);
-    int *sortArr = mergeSort(arr1 ,size1 , arr2 , size2);
+        arr2 = sort(arr2,0, size2, func);
+    int *sortArr = mergeSort(arr1 ,size1 , arr2 , size2, func);
     free(arr1);
     free(arr2);
     return sortArr;
@@ -57,6 +57,12 @@ int* partition(int arr[], int start, int end){
 int inverse(int a){
     return -1*a;
 }
+
+int normal(int a){
+    return a;
+}
+
+
 void printArray(int *arr, int size){
     for(int i=0; i<size; ++i)
         printf("%d, ",arr[i]);
--- c/map_0/solution1/solution1.h
+++ c/map_0/solution1/solution1.h
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * mergeSort(int *arr1 ,int size1 ,int *arr2 , int size2);
-int *sort(int arr[], int start, int end);
+//int * mergeSort(int *arr1 ,int size1 ,int *arr2 , int size2);
+//int *sort(int arr[], int start, int end);
 int* partition(int arr[], int start, int end);
 void printArray(int *arr, int size);
+int inverse(int a);
+int normal(int a);
+int * mergeSort(int *arr1 ,int size1 ,int *arr2 , int size2, int (*fanc)(int));
+int *sort(int arr[], int start, int end,int (*fanc)(int));
