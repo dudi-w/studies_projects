a077a272c7794417b105d9048f95eaabc44b3dc6
dudi-w <dudi.w9453@gmail.com>
2023-01-25 11:35:59 +0200

    update

--- cpp/04-polynomial/dataStructure.hpp
+++ cpp/04-polynomial/dataStructure.hpp
@@ -3,8 +3,11 @@
 #include <memory.h>
 #include <istream>
 
+class ArrByte;
+
 namespace ds
 {
+
 class ArrRational
 {
     public:
@@ -18,7 +21,6 @@ class ArrRational
         Rational& operator[](size_t i);
         const Rational& operator[](size_t i) const;
 
-        
         size_t getSize() const;
         void display() const;
 
@@ -29,24 +31,23 @@ class ArrRational
 
 class ArrByte
 {
-public:
-    explicit ArrByte(size_t size);
-    ArrByte(const ArrByte& other);
-    ~ArrByte();
+    public:
+        explicit ArrByte(size_t size);
+        ArrByte(const ArrByte& other);
+        ~ArrByte();
 
-    
-    ArrByte& operator=( const ArrByte& other);
-    ArrByte operator+( const ArrByte& other) const;
-    ArrByte& operator+=( const ArrByte& other);
-    int8_t& operator[](size_t i);
-    const int8_t& operator[](size_t i) const;
+        ArrByte& operator=( const ArrByte& other);
+        ArrByte operator+( const ArrByte& other) const;
+        ArrByte& operator+=( const ArrByte& other);
+        int8_t& operator[](size_t i);
+        const int8_t& operator[](size_t i) const;
 
-    void cleanMamory() const;
-    void printArr() const;
-    size_t getSize() const;
+        void cleanMamory() const;
+        void display() const;
+        size_t getSize() const;
 
-public:
-    size_t m_size;
-    int8_t* m_arr;
+    public:
+        size_t m_size;
+        int8_t* m_arr;
 };
 }
