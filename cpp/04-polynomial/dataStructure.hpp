8cf1ef080c1c25e77bdb91566691f918896ca6c8
dudi-w <dudi.w9453@gmail.com>
2023-01-31 11:09:46 +0200

    CR

--- cpp/04-polynomial/dataStructure.hpp
+++ cpp/04-polynomial/dataStructure.hpp
@@ -11,23 +11,23 @@ namespace ds
 
 class ArrRational
 {
-    public:
-        explicit ArrRational();
-        explicit ArrRational(size_t m_size);
-        explicit ArrRational(size_t m_size , const Rational* const arr);
-        ArrRational(const ArrRational& other);
-        ~ArrRational();
-        
-        ArrRational& operator=( const ArrRational& other);
-        Rational& operator[](size_t i);
-        const Rational& operator[](size_t i) const;
+public:
+    explicit ArrRational();
+    explicit ArrRational(size_t m_size);
+    explicit ArrRational(size_t m_size , const Rational* const arr);
+    ArrRational(const ArrRational& other);
+    ArrRational& operator=( const ArrRational& other);
+    ~ArrRational();
+    
+    Rational& operator[](size_t i);
+    const Rational& operator[](size_t i) const;
 
-        size_t getSize() const;
-        void display() const;
+    size_t getSize() const;
+    void display() const;
 
-    private:
-        size_t m_size;
-        Rational* m_arr;
+private:
+    size_t m_size;
+    Rational* m_arr;
 };
 
 }
--- cpp/04-polynomial/polynomial.hpp
+++ cpp/04-polynomial/polynomial.hpp
@@ -6,28 +6,27 @@ namespace poly
 {
 class Polynomial
 {
-    public:
-        explicit Polynomial();
-        explicit Polynomial(size_t degree);
-        explicit Polynomial(size_t degree , const Rational* const arr);
+public:
+    explicit Polynomial();
+    explicit Polynomial(size_t degree);
+    explicit Polynomial(size_t degree , const Rational* const arr);
 
-        Polynomial operator+( const Polynomial& other) const;
-        Polynomial operator-( const Polynomial& other) const;
-        Polynomial operator*( const Polynomial& other) const;
-        Polynomial operator/( const Polynomial& other) const;
+    Polynomial operator+( const Polynomial& other) const;
+    Polynomial operator-( const Polynomial& other) const;
+    Polynomial operator*( const Polynomial& other) const;
+    Polynomial operator/( const Polynomial& other) const;
 
-        // Rational& at(size_t i) const;
-        Rational& at(size_t i);
-        float valueAt(double x) const;
-        Rational valueAt(Rational x) const;
-        Polynomial derivative() const;
-        void displye() const;
+    Rational& at(size_t i);
+    float valueAt(double x) const;
+    Rational valueAt(Rational x) const;
+    Polynomial derivative() const;
+    void displye() const;
 
-    private:
-        void reSize();
+private:
+    void reSize();
 
-    private:
-        size_t m_degree;
-        ds::ArrRational m_coeff;
+private:
+    size_t m_degree;
+    ds::ArrRational m_coeff;
 };
 }
