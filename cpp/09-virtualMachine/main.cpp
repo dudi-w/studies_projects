953c5dbb7f1bea5115c137abbecf0fa403583249
dudi-w <dudi.w9453@gmail.com>
2023-02-12 23:06:11 +0200

    edded factorial test end print report object

--- cpp/09-virtualMachine/main.cpp
+++ cpp/09-virtualMachine/main.cpp
@@ -1,5 +1,6 @@
 #include "VirtualMachine.hpp"
 
+
 void test_1(vm::VirtualMachine& myMachine)
 {
     std::vector<int32_t> code (5);
@@ -18,8 +19,8 @@ void test_1(vm::VirtualMachine& myMachine)
     code.push_back( static_cast<int32_t>(vm::OpCode::Jnz));
     code.push_back(0);
     code.push_back(static_cast<int32_t>(vm::OpCode::Halt));
-    myMachine.run(code);
-    std::cout<<'\n';
+    vm::Report report = myMachine.run(code);
+    std::cout<<report<<'\n';
 }
 
 void test_print_ABC(vm::VirtualMachine& myMachine){
@@ -37,8 +38,8 @@ void test_print_ABC(vm::VirtualMachine& myMachine){
     code.push_back(static_cast<int32_t>(vm::OpCode::Jnz));
     code.push_back(3);
     code.push_back(static_cast<int32_t>(vm::OpCode::Halt));
-    myMachine.run(code);
-    std::cout<<'\n';
+    vm::Report report = myMachine.run(code);
+    std::cout<<report<<'\n';
 }
 
 void test_print_AB_Phase3(vm::VirtualMachine& myMachine){
@@ -60,8 +61,42 @@ void test_print_AB_Phase3(vm::VirtualMachine& myMachine){
     code.push_back(static_cast<int32_t>(vm::OpCode::Dup));
     code.push_back(static_cast<int32_t>(vm::OpCode::PrintC));
     code.push_back(static_cast<int32_t>(vm::OpCode::Ret));
-    myMachine.run(code);
-    std::cout<<'\n';
+    vm::Report report = myMachine.run(code);
+    std::cout<<report<<'\n';
+}
+
+void test_factorial(vm::VirtualMachine& myMachine)
+{
+    int n = 5;
+    std::cout << "test_factorial:\nresulte for "<<n<<" = ";
+
+    std::vector<int32_t> code = {
+        static_cast<int32_t>(vm::OpCode::Push), 
+        n, 
+        static_cast<int32_t>(vm::OpCode::Call),
+        8,
+        static_cast<int32_t>(vm::OpCode::Nop),
+        static_cast<int32_t>(vm::OpCode::Print),
+        static_cast<int32_t>(vm::OpCode::Halt),
+        static_cast<int32_t>(vm::OpCode::Nop),
+        static_cast<int32_t>(vm::OpCode::Dup),//8
+        static_cast<int32_t>(vm::OpCode::Jz),
+        15,
+        static_cast<int32_t>(vm::OpCode::Dup),
+        static_cast<int32_t>(vm::OpCode::Dec),
+        static_cast<int32_t>(vm::OpCode::Call),
+        8,
+        static_cast<int32_t>(vm::OpCode::Dup),//15
+        static_cast<int32_t>(vm::OpCode::Jnz),
+        21,
+        static_cast<int32_t>(vm::OpCode::Inc),
+        static_cast<int32_t>(vm::OpCode::Push),
+        1,
+        static_cast<int32_t>(vm::OpCode::Mul),//21 
+        static_cast<int32_t>(vm::OpCode::Ret),
+    };
+    vm::Report report = myMachine.run(code);
+    std::cout<<report<<'\n';
 }
 
 int main(){
@@ -69,5 +104,7 @@ int main(){
     test_1(myMachine);
     test_print_ABC(myMachine);
     test_print_AB_Phase3(myMachine);
+    test_factorial(myMachine);
+
     return 0;
 }
