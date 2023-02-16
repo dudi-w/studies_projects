ce06b21c789441d72129f3597ce792850d518d90
dudi-w <dudi.w9453@gmail.com>
2023-02-16 10:41:22 +0200

    WIP

--- cpp/11-expression_tree/storingToExpressionTree.cpp
+++ cpp/11-expression_tree/storingToExpressionTree.cpp
@@ -6,7 +6,11 @@
 #include "tokens/operatorToken.hpp"
 #include "tokens/numToken.hpp"
 
-bool isNum(char){return false;}
+bool isOperator(char op)
+{
+    return (op == '+' || op == '-' || op == '*' || op == '/' );
+}
+
 int extractData(std::string const& strExpr, size_t& index)
 {
     size_t begin = index;
@@ -14,104 +18,113 @@ int extractData(std::string const& strExpr, size_t& index)
     {
         ++index;
     }
-    int data = std::atoi(strExpr.substr(begin, index));
+
+    int data = std::stoi(strExpr.substr(begin, index+1));
+
     return data;
 }
 
-std::vector<tk::Token> tokenizer(std::string const& strExpr)
+std::vector<std::unique_ptr<tk::Token>> tokenizer(std::string const& strExpr)
 {
-    std::vector<tk::Token> tokenExpr;
+    std::vector<std::unique_ptr<tk::Token>> tokenExpr;
     for(size_t i = 0; i<strExpr.size() ;++i){
-        if(strExpr[i] == ' ') {continue;}
-        if(!isdigit(strExpr[i])){
+        if(strExpr[i] == ' '){
+            continue;
+        }
+        
+        if(isOperator(strExpr[i])){
             Group group =  et::preurityGroup[strExpr[i]];
             auto func =  et::bildFunctions[strExpr[i]];
-            tk::OperatorToken oper(i, func, group);
-            tokenExpr.push_back(oper);
-        }else if(isNum(strExpr[i])){
+            auto oper =  std::make_unique<tk::OperatorToken>(i, func, group);
+            tokenExpr.push_back(std::move(oper));
+        
+        }else if(isdigit(strExpr[i])){
             int data = extractData(strExpr, i);
-            tk::NumToken num(i, data, Group::NumGroup);
-            tokenExpr.push_back(num);
-        }else{assert(!"invalid expressin");}
+            auto num =  std::make_unique<tk::NumToken>(i, data, Group::NumGroup);
+            tokenExpr.push_back(std::move(num));
+        
+        }else{
+            assert(!"invalid expressin");
+        }
     }
     return tokenExpr;
 }
 
-void parser(std::string const& expression , et::ExpressionNode* root , size_t& count)
-{
-    if(root){
-        abort();
-    }
-
-    if(expression.size()==1 && isdigit(expression[0])){
-        root = creatNode(static_cast <int> (expression[0]));
-        ++count;
-        return;
-    }
-
-    size_t i = preurity(expression);
-    et::OperatorNode* pNode = creatNode(expression[i]);
-    root = pNode;
-    ++count;
-
-    parser(expression.substr(0,i) ,pNode->left() ,count);
-    parser(expression.substr(i+1,expression.size()) ,pNode->right() ,count);
-    return;
-}
-
-size_t preurity(std::string const& expression)
-{
-    size_t i;
-    std::string::const_iterator it = expression.cbegin();
-    size_t min = INFINITY;
-    while (it != expression.cend()){
-        min = std::min(rating(*it), min);///////????????
-    }
-        
-    return i;
-}
-
-unsigned int rating(char q)
-{
-    switch (q){
-        case '+':
-            return 1;
-
-        case '-':
-            return 1;
-
-        case '*':
-            return 2;
-
-        case '/':
-            return 2;
-
-        default:
-            return INFINITY;
-    }
-}
-et::OperatorNode* creatNode(char preurity)
-{
-    switch (preurity)
-    {
-        case '+':
-            return new et::AddNode;
-        
-        case '-':
-            return new et::SubNode;
-
-        case '*':
-            return new et::MulNode;
-
-        case '/':
-            return new et::DivNode;
-
-        default:
-            abort();
-    }
-}
-
-et::NumNode* creatNode(int num)
-{
-    return new et::NumNode(num);
-}
+// void parser(std::string const& expression , et::ExpressionNode* root , size_t& count)
+// {
+    // if(root){
+        // abort();
+    // }
+// 
+    // if(expression.size()==1 && isdigit(expression[0])){
+        // root = creatNode(static_cast <int> (expression[0]));
+        // ++count;
+        // return;
+    // }
+// 
+    // size_t i = preurity(expression);
+    // et::OperatorNode* pNode = creatNode(expression[i]);
+    // root = pNode;
+    // ++count;
+// 
+    // parser(expression.substr(0,i) ,pNode->left() ,count);
+    // parser(expression.substr(i+1,expression.size()) ,pNode->right() ,count);
+    // return;
+// }
+// 
+// size_t preurity(std::string const& expression)
+// {
+    // size_t i;
+    // std::string::const_iterator it = expression.cbegin();
+    // size_t min = INFINITY;
+    // while (it != expression.cend()){
+        // min = std::min(rating(*it), min);///////????????
+    // }
+        // 
+    // return i;
+// }
+// 
+// unsigned int rating(char q)
+// {
+    // switch (q){
+        // case '+':
+            // return 1;
+// 
+        // case '-':
+            // return 1;
+// 
+        // case '*':
+            // return 2;
+// 
+        // case '/':
+            // return 2;
+// 
+        // default:
+            // return INFINITY;
+    // }
+// }
+// et::OperatorNode* creatNode(char preurity)
+// {
+    // switch (preurity)
+    // {
+        // case '+':
+            // return new et::AddNode;
+        // 
+        // case '-':
+            // return new et::SubNode;
+// 
+        // case '*':
+            // return new et::MulNode;
+// 
+        // case '/':
+            // return new et::DivNode;
+// 
+        // default:
+            // abort();
+    // }
+// }
+// 
+// et::NumNode* creatNode(int num)
+// {
+    // return new et::NumNode(num);
+// }
