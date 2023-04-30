715f76f9515c1da43fba30be52e499ba0223862f
dudi-w <dudi.w9453@gmail.com>
2023-04-30 17:37:46 +0300

    rename

--- cpp/prj-02-koogle/mainClient.cpp
+++ cpp/prj-02-koogle/mainClient.cpp
@@ -7,7 +7,7 @@ int main()
     se::ClientQueryHandler queryHandler("127.0.0.1", 8080);
     se::TextUI TUI;
     se::SearchQueryFacilita queryFacilitator(queryHandler, TUI);
-    queryFacilitator.strart();
+    queryFacilitator.start();
 
     return EXIT_SUCCESS;
 }
