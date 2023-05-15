c20a41914df21a7227133294e44acb04b77d79c1
dudi-w <dudi.w9453@gmail.com>
2023-05-15 16:51:15 +0300

    add using & remove usealss includes

--- cpp/prj-02-koogle/includes/matabase.hpp
+++ cpp/prj-02-koogle/includes/matabase.hpp
@@ -4,12 +4,11 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <mutex>
-#include <shared_mutex>
 
 #include "safeUnorderedTable.hpp"
 #include "setDatabase.hpp"
 #include "getDatabase.hpp"
+#include "pageRank.hpp"
 
 namespace se{//Search Engine
 
@@ -24,7 +23,7 @@ public:
     Matabase& operator=(Matabase const& other) = delete;
     virtual ~Matabase() = default;
 
-    virtual std::unordered_map<std::string ,size_t> getLinkOfWord(StrC word) const override;
+    virtual se::LinkVec getLinkOfWord(StrC word) const override;
     virtual size_t getLinkOccurrenceCount(StrC link1, StrC link2) const override;
     virtual void insertLinks(StrC srclink, std::vector<std::string> const& links) override;
     virtual void insertWords(StrC srcLink, std::vector<std::string> const& words) override;
@@ -38,6 +37,7 @@ private:
 private:
     se::MyMap m_safeWordsIndex;
     se::MyMap m_safeLinksMap;
+    se::PageRank m_pageRank;
 };
 
 }//namespace se
