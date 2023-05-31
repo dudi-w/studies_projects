64130edec0c2e4488a39e78738d72e85eb56fe24
dudi-w <dudi.w9453@gmail.com>
2023-05-31 09:02:29 +0300

    reorganize

--- cpp/prj-02-koogle/includes/safeDataStructures/safeQueue.hpp
+++ cpp/prj-02-koogle/includes/safeDataStructures/safeQueue.hpp
@@ -20,16 +20,13 @@ public:
 public:
     void deQueue(T& element);
     void inQueue(const T& element);
-    void shutdown();
-    // void unsafeInQueue(const T& elements);//!
     void inQueue(std::vector<T> const& elements);
+    void shutdown();
 
 private:
     std::queue<T> m_queue;
     std::mutex m_mutex;
-    // std::atomic<size_t> m_waiting;
     std::condition_variable m_cv;
-    // const size_t m_maxThreads;
     std::function<void(void)> m_sleep;
     std::function<void(void)> m_waekup;
     std::atomic<bool> m_shutdown;
